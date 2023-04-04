477de342588179f36bb729e33a3db8294c8f6939
Mustafa Alsolami <malsolam@udel.edu>
2023-04-04 16:39:32 -0400

    added TestASTOPtimizer.kt

--- src/main/kotlin/edu/udel/blc/ast/opt/TestASTOptimizer.kt
+++ src/main/kotlin/edu/udel/blc/ast/opt/TestASTOptimizer.kt
@@ -1,4 +1,22 @@
 package edu.udel.blc.ast.opt
 
-class TestASTOptimizer {
+import edu.udel.blc.ast.BinaryExpressionNode
+import edu.udel.blc.ast.BinaryOperator
+import edu.udel.blc.ast.IntLiteralNode
+
+private fun additionTest(): Unit{
+    val node = BinaryExpressionNode(
+        range = IntRange.EMPTY,
+        operator = BinaryOperator.ADDITION,
+        left = IntLiteralNode(IntRange.EMPTY, 5),
+        right = IntLiteralNode(IntRange.EMPTY, 5)
+    )
+    val optimized = ExpressionOptimizer().apply(node)
+    val expected = IntLiteralNode(-1..-1, 10)
+
+    check( optimized == expected)
+}
+
+fun main() {
+    additionTest()
 }
