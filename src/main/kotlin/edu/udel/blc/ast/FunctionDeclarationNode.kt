18ca7f0ecbf59dc0168afb51b7ecf7c27d5fdc4b
Mustafa Alsolami <malsolam@udel.edu>
2023-04-05 19:34:38 -0400

    implemented equals() for FunctionDeclarationNode

--- src/main/kotlin/edu/udel/blc/ast/FunctionDeclarationNode.kt
+++ src/main/kotlin/edu/udel/blc/ast/FunctionDeclarationNode.kt
@@ -9,7 +9,11 @@ class FunctionDeclarationNode(
     val body: BlockNode,
 ) : Node {
     override fun equals(other: Any?): Boolean {
-        TODO("Not yet implemented")
+        return (other?.let {it is FunctionDeclarationNode &&
+                it.range == this.range &&
+                it.name == this.name &&
+                it.returnType == this.returnType &&
+                it.body == this.body } == true)
     }
 
     override fun toString(): String {
