5696adb4919e3dde684e828791cd84daed40d237
Mustafa Alsolami <malsolam@udel.edu>
2023-04-05 19:36:23 -0400

    implemented equals() for VariableDeclarationNode

--- src/main/kotlin/edu/udel/blc/ast/VariableDeclarationNode.kt
+++ src/main/kotlin/edu/udel/blc/ast/VariableDeclarationNode.kt
@@ -8,7 +8,11 @@ class VariableDeclarationNode(
     val initializer: Node,
 ) : Node {
     override fun equals(other: Any?): Boolean {
-        TODO("Not yet implemented")
+        return (other?.let {it is VariableDeclarationNode &&
+                it.range == this.range &&
+                it.name == this.name &&
+                it.type == this.type &&
+                it.initializer == this.initializer} == true)
     }
 
     override fun toString(): String {
