82d36139fc022981725af1cf1f1ebd96ab321cf2
Mustafa Alsolami <malsolam@udel.edu>
2023-04-05 21:42:01 -0400

    implemented toString() for StringLiteralNode

--- src/main/kotlin/edu/udel/blc/ast/StringLiteralNode.kt
+++ src/main/kotlin/edu/udel/blc/ast/StringLiteralNode.kt
@@ -12,6 +12,7 @@ class StringLiteralNode(
     }
 
     override fun toString(): String {
-        TODO("Not yet implemented")
+        return "{StringLiteralNode (range = $range," +
+                "value = $value,)}"
     }
 }
