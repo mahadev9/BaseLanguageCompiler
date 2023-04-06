648e1167be9c673b45acc7a67fc0d9398ea1d039
Mustafa Alsolami <malsolam@udel.edu>
2023-04-05 21:41:14 -0400

    implemented toString() for ParameterNode

--- src/main/kotlin/edu/udel/blc/ast/ParameterNode.kt
+++ src/main/kotlin/edu/udel/blc/ast/ParameterNode.kt
@@ -14,6 +14,8 @@ class ParameterNode(
     }
 
     override fun toString(): String {
-        TODO("Not yet implemented")
+        return "{ParameterNode (range = $range," +
+                "name = $name," +
+                "type = ${type.toString()})}"
     }
 }
